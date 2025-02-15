a8ec85bcb759be3ecf84a6f927a96747f5c22571
marlonceub <marlonmoretti@sempreceub.com>
2025-02-15 17:38:16 -0300

    teste

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Teste 02');
+  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Teste 002');
 };
 
 const server = http.createServer(requestHandler);
