8766b9cccc9d2ea7dc66efb3c916093b5cf5b963
Marlon <marlonmoretti@sempreceub.com>
2025-03-04 19:26:06 -0300

    Descrição das alterações

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Teste 002');
+  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Teste 002 - Marlon');
 };
 
 const server = http.createServer(requestHandler);
